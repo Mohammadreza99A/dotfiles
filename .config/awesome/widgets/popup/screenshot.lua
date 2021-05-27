4d5c1f088f34681dab60cf08322a648567d973a8
Mohammadreza Amini <mohammadreza99a@yahoo.com>
2021-05-28 01:18:22 +0200

    mapped Print key to launch screenshot popup and changed some colors on popup

--- .config/awesome/widgets/popup/screenshot.lua
+++ .config/awesome/widgets/popup/screenshot.lua
@@ -1,54 +1,67 @@
-local wibox = require("wibox")
-local gears = require("gears")
-local awful = require("awful")
-local beautiful = require("beautiful")
-local filesystem = require("gears").filesystem
-local naughty = require("naughty")
-local create_button = require("widgets.create-button")
-local clickable_container = require("widgets.clickable-container")
-local helpers = require("lib.helpers")
+-- Mohammadreza Amini
+-- https://github.com/Mohammadreza99A
+-- screenshot.lua --> Screenshot popup bar for awesome wm
+--
+
+local wibox = require('wibox')
+local gears = require('gears')
+local awful = require('awful')
+local beautiful = require('beautiful')
+local filesystem = require('gears').filesystem
+local naughty = require('naughty')
+local create_button = require('widgets.create-button')
+local clickable_container = require('widgets.clickable-container')
+local helpers = require('lib.helpers')
 local dpi = beautiful.xresources.apply_dpi
 
 local delay_sec = 0
-local snap_area = "area"
+local snap_area = 'area'
 
-local snap_area_button = create_button.button_with_label("Selection",
-                                                         beautiful.icon_crop)
-local snap_full_button = create_button.button_with_label("Screen",
-                                                         beautiful.icon_screen)
-local snap_window = create_button.button_with_label("Window",
-                                                    beautiful.icon_window)
-snap_area_button.bg = beautiful.bg_focus
-snap_full_button.bg = beautiful.bg_normal
-snap_window.bg = beautiful.bg_dark
+local snap_area_button = create_button.button_with_label('Selection', beautiful.icon_crop)
+local snap_full_button = create_button.button_with_label('Screen', beautiful.icon_screen)
+local snap_window = create_button.button_with_label('Window', beautiful.icon_window)
+snap_area_button.bg = beautiful.bg_dark
+snap_full_button.bg = beautiful.bg_focus
+snap_window.bg = beautiful.bg_focus
 
-snap_area_button:connect_signal("button::press", function(self, _, _, button)
-    if button == 1 then
-        snap_area = 'area'
-        self.bg = beautiful.bg_focus
-        snap_full_button.bg = beautiful.bg_dark
-        snap_window.bg = beautiful.bg_dark
+snap_area_button:connect_signal(
+    'button::press',
+    function(self, _, _, button)
+        if button == 1 then
+            snap_area = 'area'
+            self.bg = beautiful.bg_dark
+            snap_full_button.bg = beautiful.bg_focus
+            snap_window.bg = beautiful.bg_focus
+        end
     end
-end)
+)
 
-snap_full_button:connect_signal("button::press", function(self, _, _, button)
-    if button == 1 then
-        snap_area = 'screen'
-        self.bg = beautiful.bg_focus
-        snap_area_button.bg = beautiful.bg_dark
-        snap_window.bg = beautiful.bg_dark
+snap_full_button:connect_signal(
+    'button::press',
+    function(self, _, _, button)
+        if button == 1 then
+            snap_area = 'screen'
+            self.bg = beautiful.bg_dark
+            snap_area_button.bg = beautiful.bg_focus
+            snap_window.bg = beautiful.bg_focus
+        end
     end
-end)
+)
 
-snap_window:connect_signal("button::press", function(self, _, _, button)
-    if button == 1 then
-        snap_area = 'window'
-        self.bg = beautiful.bg_focus
-        snap_area_button.bg = beautiful.bg_dark
-        snap_full_button.bg = beautiful.bg_dark
+snap_window:connect_signal(
+    'button::press',
+    function(self, _, _, button)
+        if button == 1 then
+            snap_area = 'window'
+            self.bg = beautiful.bg_dark
+            snap_area_button.bg = beautiful.bg_focus
+            snap_full_button.bg = beautiful.bg_focus
+        end
     end
-end)
-local button_capture = wibox.widget {
+)
+
+local button_capture =
+    wibox.widget {
     widget = clickable_container,
     {
         widget = wibox.container.background,
@@ -62,15 +75,14 @@ local button_capture = wibox.widget {
             right = dpi(10),
             top = dpi(6),
             bottom = dpi(6),
-            {widget = wibox.widget.textbox, text = "Take Screenshot"}
+            {widget = wibox.widget.textbox, text = 'Take Screenshot'}
         }
-
     }
 }
 
 local btn_delay_num = function()
-
-    local btn_plus = wibox.widget {
+    local btn_plus =
+        wibox.widget {
         {
             image = beautiful.icon_add,
             resize = true,
@@ -81,7 +93,8 @@ local btn_delay_num = function()
         widget = clickable_container
     }
 
-    local btn_minus = wibox.widget {
+    local btn_minus =
+        wibox.widget {
         {
             image = beautiful.icon_minus,
             resize = true,
@@ -92,11 +105,13 @@ local btn_delay_num = function()
         widget = clickable_container
     }
 
-    local value_text = wibox.widget {
+    local value_text =
+        wibox.widget {
         text = delay_sec,
         widget = wibox.widget.textbox
     }
-    local btn_delay = wibox.widget {
+    local btn_delay =
+        wibox.widget {
         widget = clickable_container,
         {
             widget = wibox.container.background,
@@ -118,30 +133,37 @@ local btn_delay_num = function()
                     layout = wibox.layout.fixed.horizontal
                 }
             }
-
         }
     }
 
-    btn_plus:connect_signal("button::press", function(_, _, _, button)
-        if button == 1 then
-            delay_sec = delay_sec + 1
-            value_text:set_text(delay_sec)
+    btn_plus:connect_signal(
+        'button::press',
+        function(_, _, _, button)
+            if button == 1 then
+                delay_sec = delay_sec + 1
+                value_text:set_text(delay_sec)
+            end
         end
-    end)
+    )
 
-    btn_minus:connect_signal("button::press", function(_, _, _, button)
-        if button == 1 then
-            if delay_sec < 1 then return end
-            delay_sec = delay_sec - 1
-            value_text:set_text(delay_sec)
+    btn_minus:connect_signal(
+        'button::press',
+        function(_, _, _, button)
+            if button == 1 then
+                if delay_sec < 1 then
+                    return
+                end
+                delay_sec = delay_sec - 1
+                value_text:set_text(delay_sec)
+            end
         end
-    end)
+    )
 
     return btn_delay
-
 end
 
-local btn_close = wibox.widget {
+local btn_close =
+    wibox.widget {
     {
         widget = wibox.container.background,
         bg = beautiful.bg_focus,
@@ -163,14 +185,16 @@ local btn_close = wibox.widget {
     widget = clickable_container
 }
 
-local row_top = wibox.widget {
-    {button_capture, spacing = dpi(4), layout = wibox.layout.fixed.horizontal},
+local row_top =
+    wibox.widget {
     nil,
+    button_capture,
     btn_close,
     layout = wibox.layout.align.horizontal
 }
 
-local row_middle = wibox.widget {
+local row_middle =
+    wibox.widget {
     snap_area_button,
     snap_full_button,
     snap_window,
@@ -178,13 +202,14 @@ local row_middle = wibox.widget {
     widget = wibox.layout.fixed.horizontal
 }
 
-local row_bottom = wibox.widget {
+local row_bottom =
+    wibox.widget {
     widget = wibox.container.background,
     bg = beautiful.bg_focus,
     shape = beautiful.widget_shape,
     {
         {
-            {text = "Delay in Seconds", widget = wibox.widget.textbox},
+            {text = 'Delay in Seconds', widget = wibox.widget.textbox},
             nil,
             {
                 btn_delay_num(),
@@ -198,7 +223,8 @@ local row_bottom = wibox.widget {
     }
 }
 
-local container = wibox.widget {
+local container =
+    wibox.widget {
     {
         row_top,
         row_middle,
@@ -210,27 +236,38 @@ local container = wibox.widget {
     widget = wibox.container.margin
 }
 
-local maim_popup = awful.popup {
+local maim_popup =
+    awful.popup {
     widget = {
         widget = wibox.container.background,
         shape = beautiful.widget_shape,
         bg = beautiful.bg_normal,
+        border_color = beautiful.red,
         container
     },
+    border_color = beautiful.fg_focus,
+    border_width = 2,
     ontop = true,
     visible = false,
-    bg = "#00000000",
+    bg = beautiful.bg_normal,
+    shape = gears.shape.rounded_rect,
     placement = awful.placement.centered
 }
 
-for s in screen do s.maim_popup = maim_popup end
+for s in screen do
+    s.maim_popup = maim_popup
+end
 
 local hide_maim_popup = function()
-    for s in screen do s.maim_popup.visible = not s.maim_popup.visible end
+    for s in screen do
+        s.maim_popup.visible = not s.maim_popup.visible
+    end
 end
 
 local show_maim_popup = function()
-    for s in screen do s.maim_popup.visible = true end
+    for s in screen do
+        s.maim_popup.visible = true
+    end
 end
 
 local display_popup = function()
@@ -244,72 +281,78 @@ local display_popup = function()
 end
 
 local take_screen_shot = function()
-    awesome.emit_signal("control-center::hide")
     helpers.sleep(.2)
 
-    local screen_shot_dir = "~/Pictures/Screenshots/"
+    local screen_shot_dir = '~/Pictures/Screenshots/'
 
     if not filesystem.dir_readable(screen_shot_dir) then
-        awful.spawn.with_shell("mkdir -p " .. screen_shot_dir)
+        awful.spawn.with_shell('mkdir -p ' .. screen_shot_dir)
     end
 
-    local file_name = os.date("%d-%m-%Y-%H:%M:%S") .. ".png"
-    local command = ""
+    local file_name = os.date('%d-%m-%Y-%H:%M:%S') .. '.png'
+    local command = ''
     if snap_area == 'area' then
-        command = "maim -s -d " .. delay_sec .. " " .. screen_shot_dir ..
-                      file_name
+        command = 'maim -s -d ' .. delay_sec .. ' ' .. screen_shot_dir .. file_name
     elseif snap_area == 'screen' then
-        command = "maim -d " .. delay_sec .. " " .. screen_shot_dir .. file_name
-    elseif snap_area == "window" then
-        command = "maim -d " .. delay_sec .. " " ..
-                      "-i $(xdotool getactivewindow) " .. screen_shot_dir ..
-                      file_name
+        command = 'maim -d ' .. delay_sec .. ' ' .. screen_shot_dir .. file_name
+    elseif snap_area == 'window' then
+        command = 'maim -d ' .. delay_sec .. ' ' .. '-i $(xdotool getactivewindow) ' .. screen_shot_dir .. file_name
     end
 
     hide_maim_popup()
     helpers.sleep(delay_sec + 0.2)
 
-    local open_file_cb = function()
-        awful.spawn.with_shell('xdg-open ' .. screen_shot_dir .. file_name,
-                               false)
-    end
+    awful.spawn.easy_async_with_shell(
+        command,
+        function(_, _)
+            naughty.notify {
+                app_name = 'Screenshot Tool',
+                icon = beautiful.icon_screenhost_taken,
+                timeout = 10,
+                title = 'Screenshot taken',
+                text = 'Screenshot saved to ' .. screen_shot_dir .. file_name
+            }
 
-    local open_dir_cb = function()
-        awful.spawn.with_shell('xdg-open ' .. screen_shot_dir, false)
-    end
-
-    local delete_file_cb = function()
-        awful.spawn.with_shell('gio trash ' .. screen_shot_dir .. file_name,
-                               false)
-    end
-
-    awful.spawn.easy_async_with_shell(command, function(_, _)
-        naughty.notify {
-            app_name = 'Screenshot Tool',
-            icon = beautiful.icon_screenhost_taken,
-            timeout = 10,
-            title = 'Screenshot taken',
-            text = 'Screenshot saved to ' .. screen_shot_dir .. file_name
-        }
-
-        show_maim_popup()
-    end)
-
+            show_maim_popup()
+        end
+    )
 end
 
-button_capture:connect_signal("button::press", function(_, _, _, button)
-    if button == 1 then take_screen_shot() end
-end)
+button_capture:connect_signal(
+    'button::press',
+    function(_, _, _, button)
+        if button == 1 then
+            take_screen_shot()
+        end
+    end
+)
 
-btn_close:connect_signal("button::press", function(_, _, _, button)
-    if button == 1 then hide_maim_popup() end
-end)
+btn_close:connect_signal(
+    'button::press',
+    function(_, _, _, button)
+        if button == 1 then
+            hide_maim_popup()
+        end
+    end
+)
 
 local screen_shot_button = create_button.circle_big(beautiful.icon_camera)
-local label = screen_shot_button:get_children_by_id("label")[1]
-label:set_text("Take Shot")
-screen_shot_button:connect_signal("button::press", function(self, _, _, button)
-    if button == 1 then display_popup() end
-end)
+local label = screen_shot_button:get_children_by_id('label')[1]
+label:set_text('Take Shot')
+screen_shot_button:connect_signal(
+    'button::press',
+    function(self, _, _, button)
+        if button == 1 then
+            display_popup()
+        end
+    end
+)
+
+awesome.connect_signal(
+    'screenshot::show',
+    function()
+        display_popup()
+    end
+)
 
 return screen_shot_button
